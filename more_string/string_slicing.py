63fdff0bca4eca16b4b4f875a065533328ae48fc
Ashik-Akash <ashikikbalakash15@gmail.com>
2025-05-05 22:46:56 +0600

    Initial commit

--- more_string/string_slicing.py
+++ more_string/string_slicing.py
@@ -1,12 +1,14 @@
 #string slicing
 
-string = "Hello World"
+string = "Hello World. this is new world"
 hello = string[0:5]
 hello1 = string[:5] #f I know, string slicing starts from the first index, which is zero.
 print(hello)
 print(hello1)
 
 world = string [5:] #count Spece
-wrold1 = string[6:]
+world1 = string[6:]
+world2 = string[6: len(string)]
 print(world)
-print(wrold1)
+print(world1)
+print(world2)
