4e14d40b8a7ba25a1f97da2202b8a09cc1912ced
Ashik-Akash <ashikikbalakash15@gmail.com>
2025-05-06 14:06:47 +0600

    Initial commit

---
+++ control_flow_looping/elseif_or_elif.py
@@ -0,0 +1,13 @@
+
+marks = 44
+
+if marks<=100 and marks>=80:
+    print("Yours great is : A+")
+elif marks<80 and marks>=70:
+    print("Your great is : A ")
+elif marks<60 and marks>=50:
+    print("Your great is : B")
+elif marks<45 and marks>=40:
+    print("Your great is : A-")
+elif marks<33 and marks>=0:
+    print("Oh your fail")
