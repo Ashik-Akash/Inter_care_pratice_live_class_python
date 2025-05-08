9d97c9ee19e0842f13b65197e00fd56327859227
Ashik-Akash <ashikikbalakash15@gmail.com>
2025-05-09 01:16:31 +0600

    Initial commit

--- control_flow_looping/for_in2.py
+++ control_flow_looping/for_in2.py
@@ -4,7 +4,15 @@ city = ['Dhaka','Bogra','Barisal','Rajshahi','Rangpur','Sylhet']
 for item in city:
     print("The city name is :", item)
 
+
 zila = ['Bogra','Sirajganj','Noakhali','Natore','Kustia','Joypurhat']
 
 for item in zila:
-    print(item)
+    print("The Zila name is : ", item)
+
+
+
+
+upozila = ['Dhunat','Sherpur','Nondigram','Sariakandi']
+for item in upozila:
+    print("The Upozila name is : ", item)
