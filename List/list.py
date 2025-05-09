52cae1f0d7c37402a1984e55b98679bc893b4dfd
Ashik-Akash <ashikikbalakash15@gmail.com>
2025-05-09 23:58:56 +0600

    Initial commit

--- List/list.py
+++ List/list.py
@@ -2,6 +2,7 @@ city = ['Dhaka','Barisal','Khulna']
 city.append("Bogura")
 city.append("Comilla")
 city.insert(2,"Dhunat")
+city.insert(3,"Bothuabari")
 print(city[2]) #2 number index "khula"
 print(city)
 
