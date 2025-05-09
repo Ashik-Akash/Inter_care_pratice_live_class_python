92f763eaba57cbf725181d4f0cf90afb72c36473
Ashik-Akash <ashikikbalakash15@gmail.com>
2025-05-10 00:34:05 +0600

    Initial commit

--- List/index.py
+++ List/index.py
@@ -1,3 +1,4 @@
-contry = ['Bangladesh','Pakisthan','Purtugal','Turky']
-print(contry)
-print(contry.index('Bangladesh'))
+country = ['Bangladesh','Pakisthan','Purtugal','Turky']
+print(country)
+print(country.index('Bangladesh'))
+print(country.index('Turky'))
